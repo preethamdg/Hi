dd6d40a51fa7b305b5e0093b77d9db4082050119
Ravanog <149065101+Ravanog@users.noreply.github.com>
2025-02-07 00:01:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 # Links
 SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/any_support_01')
 OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Sanjudg01")
-UPDATES_LINK = environ.get('UPDATES_LINK', '')
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/any_support_01')
 FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/+y-d-YdGnh7MzNTU1')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/+y-d-YdGnh7MzNTU1")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+y-d-YdGnh7MzNTU1")
