d7f845760b86b3cc8f681f7709ca7637c9f79cf9
preethamdg <preethamdgpreethamdg5@gmail.com>
2025-02-06 19:47:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,12 +72,12 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/HA_Bots_Support')
-OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Hansaka_Anuhas")
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/HA_Bots')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/HA_Films_World')
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/HA_Bots")
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/HA_Bots")
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/any_support_01')
+OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Sanjudg01")
+UPDATES_LINK = environ.get('UPDATES_LINK', '')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/+y-d-YdGnh7MzNTU1')
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/+y-d-YdGnh7MzNTU1")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/+y-d-YdGnh7MzNTU1")
 
 # Bot settings
 DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
